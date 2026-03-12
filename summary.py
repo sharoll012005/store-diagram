599e4b67c5ad9bf559df696bbf19507c7e6d6314
sharollhernandez@gamil.com <sharollhernandez@gmail.com>
2026-03-12 01:31:08 +0000

    bugfix found

--- summary.py
+++ summary.py
@@ -1,11 +1,11 @@
 def generate_summary(sales):
     summary = {}
     general = 0
-    for v in record_sales:
+    for v in sales:
         summary.setdefault(v["product"], 0)
         summary[v["product"]] += v["units"]
         general += v["total"]
     print("\nsummary:")
     for prod, cant in summary.items():
         print(f"{prod}: {cant} units")
-    print(f"Total raised: {general}"
+    print(f"Total raised: {general}")
